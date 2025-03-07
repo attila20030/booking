ce659e44dc43a1453eef458ef9b2caad763253ae
szt3daratt <szt3daratt@SZT5-3-HP>
2025-03-07 10:09:51 +0100

    App.js kiegészítve, és a projekt rendezése

---
+++ book/src/components/Booking.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="booking"></div>
+    <div class="container">
+      <h2 class="my-5 text-center">Időpont Foglalás</h2>
+      <form>
+        <label>Kérem válasza ki milyen szolgáltatást szeretne igénybe venni:  </label>
+        <select class="valasztek">
+          <option>Pszichológus</option>
+          <option>Mükörmös</option>
+          <option>Fodrász Férfi/Női</option>
+          <option>Orvos</option>
+          <option>Műszaki vizsga</option>
+          <option>Tetováló</option>
+          <option>Hivatali ügyintézés</option>
+        </select><br>
+  
+        <label class="idopont">Kérem válassza ki az időpontot:</label>
+        <select class="valasztek1">
+          <option>08:00</option>
+          <option>09:00</option>
+          <option>10:00</option>
+          <option>11:00</option>
+          <option>12:00</option>
+          <option>13:00</option>
+          <option>14:00</option>
+          <option>15:00</option>
+          <option>16:00</option>
+        </select>
+  
+        <div class="mb-3">
+          <label for="name" class="form-label" >Név</label>
+          <input type="text" class="form-control" id="name" placeholder="pl: Varga Irény"/>
+        </div>
+        <div class="mb-3">
+          <label for="phone" class="form-label" >Telefonszám</label>
+          <input
+            type="text"
+            class="form-control"
+            id="phone"
+            required
+            pattern="^[0-9]{10}$"
+            placeholder="Pl. 0612345678"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary mb-3 ">Foglalás</button>
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  import {ref} from 'vue'
+  </script>
+  
+  <style scoped>
+  .container{
+    background-color: lightblue;
+  }
+  </style>
+    
+    
